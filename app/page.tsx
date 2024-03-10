83f54c071dc55ac50c478057716c9741cd2b5e42
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2024-03-10 21:05:33 +0530

    Added file schema and organizations from clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,38 @@
 "use client";
-import { Button } from "@/components/ui/button";
+import {Button} from "@/components/ui/button";
 import {api} from "@/convex/_generated/api";
-import { SignInButton, SignOutButton, useSession, SignedIn, SignedOut } from "@clerk/nextjs";
+import {useOrganization} from "@clerk/nextjs";
 import {useMutation, useQuery} from "convex/react";
 
 export default function Home() {
-	const createFile = useMutation(api.files.createFile)
-	const files = useQuery(api.files.getFiles)
+	const { organization } = useOrganization();
+	const files = useQuery(
+		api.files.getFiles,
+		organization?.id ? {orgId: organization.id } : "skip"
+	)
+	const createFile = useMutation(api.files.createFile) 
 
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<SignedIn>
-				<SignOutButton>
-					<Button>Sign Out</Button>
-				</SignOutButton>
-			</SignedIn>
-			<SignedOut>
-				<SignInButton>
-					<Button>Sign In</Button>
-				</SignInButton>
-			</SignedOut>
-	
 			{files?.map(file => (
-				<p
+				<div
 					key={file._id}
 				>
 					{file.name}
-				</p>
+				</div>
 			))}
 
-			<Button onClick={() => {
-				createFile({
-					name: 'Hello World!'
-				})
-			}}>
-				Upload File
+
+			<Button
+				onClick={() => {
+					if (!organization) return;
+					createFile({
+						name: "New File",
+						orgId: organization.id
+					})
+				}}
+			>
+				Create File
 			</Button>
 		</main>
 	);
--- convex/files.ts
+++ convex/files.ts
@@ -1,20 +1,34 @@
-import {v} from 'convex/values'
+import {ConvexError, v} from 'convex/values'
 import { mutation, query } from './_generated/server'
 
 export const createFile = mutation({
 	args: {
 		name: v.string(),
+		orgId: v.string(),
 	},
 	async handler(ctx, args) {
+	const identity = await ctx.auth.getUserIdentity();
+
+	console.log(identity)
+
+	if (!identity) throw new ConvexError('unauthorized')
+	
 		await ctx.db.insert('files', {
 			name: args.name,
+			orgId: args.orgId,
 		})
 	}
 })
 
 export const getFiles = query({
-	args: {},
+	args: {
+		orgId: v.string(),
+	},
 	async handler(ctx, args) {
-		return ctx.db.query('files').collect()
+		const identity = await ctx.auth.getUserIdentity();
+		if (!identity) return [];
+		return ctx.db.query('files').withIndex("by_orgId", q => (
+			q.eq('orgId', args.orgId)
+		)).collect()
 	}
 })
