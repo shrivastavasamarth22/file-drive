e069996a13fd425c3312b654bd929931ca283f7a
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2024-03-16 14:58:02 +0530

    added a delete dropdown menu to filecard

--- app/file-card.tsx
+++ app/file-card.tsx
@@ -6,16 +6,47 @@ import {
 	CardHeader,
 	CardTitle
 } from "@/components/ui/card"
-import {Doc} from "@/convex/_generated/dataModel"
 
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import {Doc} from "@/convex/_generated/dataModel"
+import {EllipsisVertical, TrashIcon} from "lucide-react"
+
+function FileCardActions() {
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger>
+				<EllipsisVertical 
+					className="w-4 h-4"
+				/>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent>
+				<DropdownMenuItem className="flex gap-1 items-center text-red-600 cursor-pointer">
+					<TrashIcon 
+						className="w-4 h-4"
+					/>
+					Delete
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	)
+}
 
 export function FileCard ({ file }: { file:  Doc<"files"> }) {
 	return (
 		<Card>
-			<CardHeader>
+			<CardHeader className="relative">
 				<CardTitle className="truncate">
 					{file.name}
 				</CardTitle>
+				<div className="absolute top-2 right-2">
+					<FileCardActions />
+				</div>
 			</CardHeader>
 			<CardContent>
 				<p>Card Content</p>
