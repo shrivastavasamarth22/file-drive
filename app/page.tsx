d925199bccd825c4bbacf71ea42a0ec2205a8248
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2024-03-16 21:58:18 +0530

    cleaned the code a little bit

--- app/page.tsx
+++ app/page.tsx
@@ -19,17 +19,18 @@ export default function Home() {
 	}
 
 	const files = useQuery(api.files.getFiles, orgId ? { orgId } : "skip");
+	const isLoading = files === undefined;
 
 	return (
 		<main className="container mx-auto pt-12">
-			{files === undefined && (
+			{isLoading && (
 				<div className="flex flex-col gap-8 w-full items-center mt-24">
 					<Loader2 className="animate-spin w-28 h-28 text-gray-500" />
 					<div className="text-2xl">Loading your files...</div>
 				</div>
 			)}
 
-			{files && files.length === 0 && (
+			{!isLoading && files.length === 0 && (
 				<div className="flex flex-col gap-8 items-center w-full mt-12">
 					<Image
 						src={"/empty.svg"}
@@ -44,7 +45,7 @@ export default function Home() {
 				</div>
 			)}
 
-			{files && files.length > 0 && (
+			{!isLoading && files.length > 0 && (
 				<>
 					<div className="flex justify-between items-center mb-8">
 						<h1 className="text-4xl font-bold">Your Files</h1>
