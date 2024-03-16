af73960655309327088cb392906e2c5a6130a4a0
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2024-03-16 21:31:34 +0530

    created an empty page state

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,52 @@
 "use client";
 
-import {api} from "@/convex/_generated/api";
-import {useOrganization, useUser} from "@clerk/nextjs";
-import {useQuery} from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useOrganization, useUser } from "@clerk/nextjs";
+import { useQuery } from "convex/react";
 
 import { UploadButton } from "./upload-button";
-import {FileCard} from "./file-card";
+import { FileCard } from "./file-card";
+import Image from "next/image";
 
 export default function Home() {
 	const organization = useOrganization();
 	const user = useUser();
 
-	
 	let orgId: string | undefined = undefined;
 	if (organization.isLoaded && user.isLoaded) {
 		orgId = organization.organization?.id ?? user.user?.id;
 	}
 
-	const files = useQuery(
-		api.files.getFiles,
-		orgId ? {orgId} : "skip"
-	)
-
+	const files = useQuery(api.files.getFiles, orgId ? { orgId } : "skip");
 
 	return (
 		<main className="container mx-auto pt-12">
-			<div className="flex justify-between items-center mb-8">
-				<h1 className="text-4xl font-bold">Your Files</h1>
-				<UploadButton />
-			</div>
-			<div className="grid grid-cols-4 gap-4">
-				{files?.map(file => (
-					<FileCard key={file._id} file={file} />
-				))}
-			</div>
+			{files && files.length === 0 ? (
+				<div className="flex flex-col gap-4 mx-auto items-center w-full mt-12">
+					<Image
+						src={"/empty.svg"}
+						alt="an image of a picture and directory icon"
+						width="300"
+						height="300"
+					/>
+					<div className="text-2xl font-bold">
+						You have no files. Upload one now!
+					</div>
+					<UploadButton />
+				</div>
+			) : (
+				<>
+					<div className="flex justify-between items-center mb-8">
+						<h1 className="text-4xl font-bold">Your Files</h1>
+						<UploadButton />
+					</div>
+					<div className="grid grid-cols-4 gap-4">
+						{files?.map((file) => (
+							<FileCard key={file._id} file={file} />
+						))}
+					</div>
+				</>
+			)}
 		</main>
 	);
 }
